be4007328242b516dcb6ef9e443db5b282a19f6c
Artem Makarov <artemailcz@gmail.com>
2017-12-09 00:07:00 +0100

    Commituji
    jasně že commituji , že

--- intro/src/components/Game.js
+++ intro/src/components/Game.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 
 export default class Game extends Component {
+ state = {
+    count : 0
+
+ }
+getClassName = () => {
+    if (this.state.count == 3) {
+        document.getElementById("changeMe").classList.add('lol');
+        if (this.state.count == 8) {
+            document.getElementById("changeMe").classList.add('lol');
+            if (this.state.count == 15) {
+                document.getElementById("changeMe").classList.add('lol');
+                if (this.state.count == 19) {
+                    document.getElementById("changeMe").classList.add('lol');
+                    if (this.state.count == 25) {
+                        document.getElementById("changeMe").classList.add('lol');
+           
+        
+    }
+}
+
+changeState = (e) => {
+    this.setState({count: this.state.count +1});
+    this.getClassName()
+}
     render() {
         return (
             <div>
-                <h1>toto je komponenta , tady bude hra </h1>
+                
+                <h1>Count : {this.state.count}</h1>
+                <button className="btn-danger" style={{fontSize:"50px"}} id="changeMe"  onClick={this.changeState}>click on me </button>
+
+
+
             </div>
         )
     }
