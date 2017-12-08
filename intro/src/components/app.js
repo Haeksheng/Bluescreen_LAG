631117680cb2ecf2d0339bf0c629c349346a166a
Artem Makarov <artemailcz@gmail.com>
2017-12-08 20:05:49 +0100

    intro up
    ...

--- intro/src/components/app.js
+++ intro/src/components/app.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 import Form from "./form"
+import $ from "jquery"
 
 export default class App extends Component {
    
 
 state = {
-  nickname: ""
+  nickname: "",
+  visibility: 'true'
 }
 
+
+handleChange = (e) => this.setState({nickname: e.target.value})
+
+
+
   render() {
-    return (<div style={{textAlign:"center",marginTop:"25vh"}}>
-      <div className="col-xs-12 intro">
-      <h1>Welcome player</h1>
-      {this.setState()}
+    
+            return (<div style={{textAlign:"center",marginTop:"25vh"}}>
+      
+      <div className={this.state.visibility}>
+        <div className="col-xs-12 intro">
+        <div className='intro-game'>
+      
+        
+        <h1>Welcome player</h1>
+        
       <h1>Choose your nickname</h1>
-      </div>
-       
-      <Form/>
-      <div className="fontf">
-      <h1 style={{marginTop: "50px"}}> You are playing as {this.state.nickname}</h1>
-      </div>
-    </div>);
+        
+        
+        </div>
+        </div>
+        <Form changer={this.handleChange}/>
+        
+
+
+        </div>
+        <div className="fontf">
+        <h1 style={{marginTop: "50px"}}> You are playing as {this.state.nickname}</h1>
+        </div>
+        
+      </div>);
+
+
   }
 }
+
+
+
+
--- intro/src/components/form.js
+++ intro/src/components/form.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
-
+import $ from "jquery"
 export default class Form extends Component {
+   
+   
+   
+   
+   
     render() {
         return (
             <div>
                 <form>
                 <label>
                     
-                     <input type="text" name="name" className="col-xs-12 formm" />
+                     <input type="text" name="name" className="col-xs-12 formm" onChange={this.props.changer}/>
                      </label>
                      <div className="col-xs-12">
-                    <input type="submit" value="Submit" className="button-submit"/>
+                    <input type="submit" value="Play the game" className="button-submit" onClick />
                     </div>
                     </form>
             </div>
